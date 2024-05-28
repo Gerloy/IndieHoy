cc6461f4e0ad506948c093d975e78ae9101e72da
Gerloy <gersilro@hotmail.com>
2024-05-27 21:02:16 -0300

    Estado de entrega 1 (Arreglar para el recuperatorio)

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,9 @@
+
+//Variables del form
+var input_nombre = document.getElementById(nombre);
+var input_mail = document.getElementById(mail);
+var input_disciplina = document.getElementById(disciplina);
+
 function abrirLink(url){
 	window.open(url, '_blank').focus();
 }
