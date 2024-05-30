0906208ca074e113c02e77109dd413c87430de9e
Gerloy <gersilro@hotmail.com>
2024-05-30 03:25:51 -0300

    Pequeño cambio al form

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
 
 //Variables del form
-var input_nombre = document.getElementById(nombre);
-var input_mail = document.getElementById(mail);
-var input_disciplina = document.getElementById(disciplina);
+var input_nombre = document.getElementById("nombre");
+var input_mail = document.getElementById("mail");
+var input_disciplina = document.getElementById("disciplina");
 
 function abrirLink(url){
 	window.open(url, '_blank').focus();
