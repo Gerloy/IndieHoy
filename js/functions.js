50a07bd6a67db402b7bd1b949f40097bd97d3c59
Gerloy <gersilro@hotmail.com>
2024-06-03 20:30:53 -0300

    actualizacion de los inputs

--- js/functions.js
+++ js/functions.js
@@ -9,8 +9,34 @@ function abrirLink(url){
 	window.open(url, '_blank').focus();
 }
 
+//Cosas  del nombre -------------------------------------------------------------------------------------------------------------------
+input_nombre.value = "Nombre";
+input_nombre.addEventListener("click", function() {
+	if (this.value == "Nombre"){
+		this.value = "";
+	}
+})
+
+input_nombre.addEventListener("focusout", function(){
+	if (this.value == ""){
+		this.value = "Nombre";
+	}
+})
 
 
+//Cosas del mail ----------------------------------------------------------------------------------------------------------------------
+input_mail.value = "Correo Electronico";
+input_mail.addEventListener("click", function() {
+	if (this.value == "Correo Electronico"){
+		this.value = "";
+	}
+})
+
+input_mail.addEventListener("focusout", function(){
+	if (this.value == ""){
+		this.value = "Correo Electronico";
+	}
+})
 //Cosas del select --------------------------------------------------------------------------------------------------------------------
 input_disciplina.value = "default";
 //Borra la opcion "Elegir disciplina del select"
